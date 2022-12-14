8c47d55e84935f86b1dd76766a86d5a7b5661fae
Chris Edwards <chrisedwards96@outlook.com>
2022-12-14 17:06:56 +0000

    Amended the growUp method to include hunger,fitness and return new age

--- src/pet.js
+++ src/pet.js
@@ -1,9 +1,17 @@
 function Pet(name) {
   this.name = name;
   this.age = 0;
+  this.hunger = 0;
+  this.fitness = 10;
 
   Pet.prototype.growUp = function () {
-    this.age = +1;
+    // Increment the age property by 1
+    this.age++;
+    console.log(name + "'s age is: " + this.age);
+    this.hunger += 5;
+    console.log(name + "'s hunger is: " + this.hunger);
+    this.fitness -= 3;
+    console.log(name + "'s fitness is: " + this.fitness);
   };
 }
 
