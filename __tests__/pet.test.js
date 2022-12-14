1481e2ac317b15659eacd285a99851ebc8f00f60
Chris Edwards <chrisedwards96@outlook.com>
2022-12-14 16:45:18 +0000

    added a growUp() prototype to increase the age by 1

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,14 +1,17 @@
-const Pet = require("../src/pet");
+const Pet = require("../src/pet.js");
 
 describe("constructor", () => {
   it("returns an object", () => {
     expect(new Pet("Fido")).toBeInstanceOf(Object);
   });
-});
 
-describe("construtor", () => {
   it("Sets the name property of the Pet", () => {
     const pet = new Pet("Fido");
     expect(pet.name).toEqual("Fido");
+  });
+
+  it("has the starting age of 0", () => {
+    const pet = new Pet("Fido");
+    expect(pet.age).toEqual(0);
   });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,7 +1,11 @@
-class Pet {
-  constructor(name) {
-    this.name = name;
-  }
+function Pet(name) {
+  this.name = name;
+  this.age = 0;
+
+  Pet.prototype.growUp = function () {
+    this.age = +1;
+  };
 }
 
+// Export the Pet class
 module.exports = Pet;
