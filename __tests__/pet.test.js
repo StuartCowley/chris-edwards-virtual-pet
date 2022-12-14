21bc5bbdaee4ec956f8ebd319b10834b942339d4
Chris Edwards <chrisedwards96@outlook.com>
2022-12-14 17:56:59 +0000

    Changed the theme of the pet project to aliens and added a fly method to increase the fitness of the alien

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,27 +1,37 @@
-const Pet = require("../src/pet.js");
+const Alien = require("../src/pet.js");
 
 describe("constructor", () => {
   it("returns an object", () => {
-    expect(new Pet("Fido")).toBeInstanceOf(Object);
+    expect(new Alien("Zorg")).toBeInstanceOf(Object);
   });
 
-  it("Sets the name property of the Pet", () => {
-    const pet = new Pet("Fido");
-    expect(pet.name).toEqual("Fido");
+  it("Sets the name property of the Alien", () => {
+    const alien = new Alien("Zorg");
+    expect(alien.name).toEqual("Zorg");
   });
 
   it("has the starting age of 0", () => {
-    const pet = new Pet("Fido");
-    expect(pet.age).toEqual(0);
+    const alien = new Alien("Zorg");
+    expect(alien.age).toEqual(0);
   });
 });
 
 describe("growUp", () => {
   it("increments the age by 1", () => {
-    const pet = new Pet("Fido");
-    pet.growUp();
-    expect(pet.age).toEqual(1);
-    expect(pet.hunger).toEqual(5);
-    expect(pet.fitness).toEqual(10);
+    const alien = new Alien("Zorg");
+    alien.growUp();
+    expect(alien.age).toEqual(1);
+    expect(alien.hunger).toEqual(5);
+    expect(alien.fitness).toEqual(7);
+  });
+});
+
+describe("fly", () => {
+  it("Increases the fitness of the alien by 4", () => {
+    const alien = new Alien("Zorg");
+    expect(alien.age).toEqual(1);
+    expect(alien.fitness).toEqual(7);
+    alien.fly();
+    expect(alien.fitness).toEqual(10);
   });
 });
--- src/pet.js
+++ src/pet.js
@@ -1,19 +1,42 @@
-function Pet(name) {
+function Alien(name) {
   this.name = name;
   this.age = 0;
   this.hunger = 0;
   this.fitness = 10;
-
-  Pet.prototype.growUp = function () {
-    // Increment the age property by 1
-    this.age++;
-    console.log(name + "'s age is: " + this.age);
-    this.hunger += 5;
-    console.log(name + "'s hunger is: " + this.hunger);
-    this.fitness -= 3;
-    console.log(name + "'s fitness is: " + this.fitness);
-  };
 }
 
-// Export the Pet class
-module.exports = Pet;
+Alien.prototype.growUp = function () {
+  // Increment the age property by 1
+  this.age++;
+  console.log(this.name + "'s age is: " + this.age);
+
+  // Increment the hunger property by 5, but not if it is already at its maximum value of 30
+  if (this.hunger < 30) {
+    this.hunger += 5;
+    console.log(this.name + "'s hunger is: " + this.hunger);
+  }
+
+  // Decrement the fitness property by 3, but not if it is already at its minimum value of 0
+  if (this.fitness > 0) {
+    this.fitness -= 3;
+    console.log(this.name + "'s fitness is: " + this.fitness);
+  }
+};
+
+Alien.prototype.fly = function () {
+  // if fitness is less than 10 increment by 4 //
+  if (this.fitness < 10) {
+    this.fitness += 4;
+    console.log(
+      this.name + " has been flying! their fitness is now " + this.fitness
+    );
+  }
+
+  // if fitness is maximum of 10 return message //
+  if (this.fitness === 10) {
+    console.log(this.name + " is fit enough for intergalactic travel!");
+  }
+};
+
+// Export the Alien class
+module.exports = Alien;
